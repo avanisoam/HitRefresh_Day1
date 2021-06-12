e9280d02c7fd381dfaf257516a236edbc92d2b79
Avani Soam <avanisoam@gmail.com>
2021-06-12 21:30:56 +0530

    Create server.js

---
+++ Node js/server.js
@@ -0,0 +1,19 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res){   // 2 - creating server
+
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>Hello from JavaScript.</p></body></html>');
+        res.end();
+    }
+
+});
+
+server.listen(5000); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
